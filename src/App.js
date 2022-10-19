4a8456b13cb4dcaaf3f5ab58a57c4c8bc30acd1e
vishwakranti <87359394+vishwakranti@users.noreply.github.com>
2022-10-19 20:40:37 +1300

    Some editing

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <p>Added some code</p>
+        <p>Added some more code</p>
+        <p>Added some even more code</p>
         <a
           className="App-link"
           href="https://reactjs.org"
